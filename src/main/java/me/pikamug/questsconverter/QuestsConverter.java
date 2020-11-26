80e69279f1e7172e1a4852708fbb01cbb278d56c
PikaMug <2267126+PikaMug@users.noreply.github.com>
2020-11-25 23:45:43 -0500

    Establish folder structure

---
+++ src/main/java/me/pikamug/questsconverter/QuestsConverter.java
@@ -0,0 +1,16 @@
+package me.pikamug.questsconverter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QuestsConverter extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+}
